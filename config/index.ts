85f1d2237fb8033a103ca744d2714012fcaf155f
silversuite <williamjang92@gmail.com>
2025-11-07 23:02:46 +0300

    update id

--- config/index.ts
+++ config/index.ts
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 
 export const siteConfig: Metadata = {
-  title: "William Taylor | Space Portfolio",
+  title: "Silver Suite | 3D & Full-Stack Dev",
   description: "Welcome to my full stack Next.js 14 space portfolio.",
   keywords: [
     "reactjs",
